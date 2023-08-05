13f1aa9959d338092ca828c4de18296fb4eb60c4
Mauro Filho <mauro.filho@gmail.com>
2023-08-05 16:30:31 -0300

    fix: set runner on .sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 export NOW=`date +"%Y-%m-%d-%H%M%S"`
 echo $NOW
-python3 check.py --runner local --project_id $GCP_WEATHER_PROJECT_ID --topic_id $GCP_WEATHER_LOCAL_TOPIC_ID > logs/$NOW.log
+python3 check.py --runner gcp --project_id $GCP_WEATHER_PROJECT_ID --topic_id $GCP_WEATHER_LOCAL_TOPIC_ID > logs/$NOW.log
