645aa5f4660726538e9634f11b4239a46e509b42
Mauro Filho <mauro.filho@gmail.com>
2023-08-05 11:08:47 -0300

    feat: check temperature and humidity using DHT22 sensor

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# weather-home-station-rasp-client
+# Weather Home Station (rasp client)
 Personal project to check temperature using a raspberry pi and a DHT22 sensor
+
+## setup
+
+### Installing requirements
+
+``pip3 install -r requirements.txt``
+
+### run
+
+``python3 check.py``
---
+++ check.py
@@ -0,0 +1,36 @@
+import sys
+import argparse
+import adafruit_dht
+import board
+
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+
+def run(args):
+    temperature = 0
+    humidity = 0
+    try:
+        temperature, humidity = check_weather(args)
+    except RuntimeError as error:
+        print(error.args[0])
+        temperature, humidity = check_weather(args) 
+    if args.runner == 'gcp':
+        post_on_pubsub(args, temperature, humidity)
+    else:
+        print('Temperature: {temperature} - Humidity: {humidity}')
+
+def check_weather(args):
+    temperature = dhtDevice.temperature
+    humidity = dhtDevice.humidity
+
+def post_on_pubsub(args, temperature, humidity):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--runner',
+        dest='runner',
+        default='local'
+    )
+    args = parser.parse_args()
+    print(args)
