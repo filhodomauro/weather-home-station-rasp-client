3e301bf7ed0c1dd5970240d00d29d6741b60ec31
Mauro Filho <mauro.filho@gmail.com>
2023-08-05 16:45:06 -0300

    feat: add logging

--- check.py
+++ check.py
@@ -2,6 +2,7 @@ import time
 import datetime
 import json
 import argparse
+import logging
 import adafruit_dht
 import board
 from google.cloud import pubsub_v1
@@ -12,7 +13,7 @@ def run(args):
         if args.runner == 'gcp':
             post_on_pubsub(args, temperature, humidity)
         else:
-            print('Temperature: {:.1f} - Humidity: {:.1f}'.format(temperature, humidity))
+            logging.info('Temperature: {:.1f} - Humidity: {:.1f}'.format(temperature, humidity))
     else:
         raise RuntimeError('Fail to get temperature')
 
@@ -20,13 +21,13 @@ def check_weather(args):
     dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
     temperature = None
     humidity = None
-    tries = 0
-    while temperature == None and tries < 10:
+    tries = 1
+    while temperature == None and tries <= 10:
         try:
             humidity = dhtDevice.humidity
             temperature = dhtDevice.temperature
         except RuntimeError as error:
-            print(error.args[0])
+            logging.error(f'Error on {tries} try. {error.args[0]}')
             tries += 1
             time.sleep(3.0)
     return temperature, humidity
@@ -40,9 +41,11 @@ def post_on_pubsub(args, temperature, humidity):
         'local_datetime': datetime.datetime.now().isoformat()
     }).encode('utf-8')
     future = publisher.publish(topic_path, data)
-    print(f'published message id {future.result()}')
+    logging.info(f'published message id {future.result()}')
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='logs/output.log', encoding='utf-8', level=logging.INFO)
+    logging.info('Starting check weather...')
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--runner',
@@ -59,6 +62,8 @@ if __name__ == '__main__':
         dest='topic_id',
         default=None
     )
+
     args = parser.parse_args()
-    print(args)
+    logging.debug(args)
     run(args)
+    logging.info('Check weather finished!')
